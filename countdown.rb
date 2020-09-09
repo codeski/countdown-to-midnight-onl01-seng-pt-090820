86d56db1eaa1a0a325e3677517cd0140ac06389a
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-09 23:04:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
-
-def countdown
+def countdown_with_sleep(n)
+  while -n <= -1 
+  puts "#{-n} SECOND(S)!"
+  -n += 1
 end
+end
+
+def countdown(n)
+  while n >= 1 
+  puts "#{n} SECCOND(S)!"
+  1.second
+  n -= 1
+end
+  returns "HAPPY NEW YEAR!"
+end
+
+
+  
